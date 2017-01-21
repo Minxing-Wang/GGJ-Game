163d1fa50e7e17dca2d2c03ac00f51360e13f676
Minxing Wang <mc_wang@live.com>
2017-01-20 21:27:01 -0800

    basic player movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.constraints = RigidbodyConstraints.FreezeRotation;
+	}
+
+	void Update ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal")  * Time.deltaTime * speed;
+		float moveVertical = Input.GetAxis ("Vertical") * Time.deltaTime * speed;
+
+		transform.Translate (0, 0, moveVertical);
+		transform.Translate (moveHorizontal, 0, 0 );
+	}
+}
