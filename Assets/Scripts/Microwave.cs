0635ac58a83a115b436d6374b67a9dece646ff27
Ao Lin <gongzhilin@hotmail.com>
2017-01-21 20:08:31 -0800

    gong chainges;

---
+++ Assets/Scripts/Microwave.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Microwave : MonoBehaviour {
+
+	bool hasCollided = false;
+	string labelText = "";
+
+	void onGUI(){
+		if (hasCollided == true) {
+			GUI.Box (new Rect (140, Screen.height - 50, Screen.width - 300, 120), (labelText));
+		}
+	}
+
+	void OnTriggerEnter(Collider c){
+		Debug.Log ("Player has entered the trigger");
+		if (c.gameObject.tag == "FPSController" || c.gameObject.tag == "FirstPersonCharacter") {
+			hasCollided = true;
+			labelText = "Hit E to use microwave!";
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		Debug.Log ("Player has exit the trigger");
+		hasCollided = false;
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
