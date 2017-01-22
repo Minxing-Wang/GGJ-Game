453111794f6a19dd03c0d599cf6c92f98a35537a
Ao Lin <gongzhilin@hotmail.com>
2017-01-22 14:33:03 -0800

    button scripts

--- Assets/Follow.cs
+++ Assets/Follow.cs
@@ -11,17 +11,21 @@ public class Follow : MonoBehaviour {
     public float smoothTime = 10.0f;
     //Vector3 used to store the velocity of the enemy
     private Vector3 smoothVelocity = Vector3.zero;
+	public Microwave mw;
+
     //Call every frame
     void Update()
     {
-        //Look at the player
-        transform.LookAt(player);
-        //Calculate distance between player
-        float distance = Vector3.Distance(transform.position, player.position);
-        //If the distance is smaller than the walkingDistance
+		if (mw.state) {
+			//Look at the player
+			transform.LookAt(player);
+			//Calculate distance between player
+			float distance = Vector3.Distance(transform.position, player.position);
+			//If the distance is smaller than the walkingDistance
 
-            //Move the enemy towards the player with smoothdamp
-            transform.position = Vector3.SmoothDamp(transform.position, player.position, ref smoothVelocity, smoothTime);
+			//Move the enemy towards the player with smoothdamp
+			transform.position = Vector3.SmoothDamp(transform.position, player.position, ref smoothVelocity, smoothTime);
+		}
     }
 }
 
