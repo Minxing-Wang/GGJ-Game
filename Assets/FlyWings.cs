b8ab2be7560d58a74a74380a787aa554709fcc1e
Dan Brod <didntknowwhattoname@gmail.com>
2017-01-22 11:57:41 -0800

    added bees, collision and follow scripts

---
+++ Assets/FlyWings.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyWings : MonoBehaviour
+{
+
+    public Transform wings;
+    float freq = 12;
+    private float upSize = 1;
+
+    private float downSize = -0.5f;
+
+    void Update()
+    {
+        float range = upSize - downSize;
+        float currSize = Mathf.PingPong(Time.time * freq, 1) * range + downSize;
+        wings.localScale = new Vector3(wings.localScale.x, wings.localScale.y, currSize);
+    }
+}
