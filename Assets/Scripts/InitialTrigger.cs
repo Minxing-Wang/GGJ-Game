3ede9621c05ed02fbcf84d3783ad857cb64a80ed
Ao Lin <gongzhilin@hotmail.com>
2017-01-21 23:01:29 -0800

    added initial food item with interaction with keyboard

---
+++ Assets/Scripts/InitialTrigger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialTrigger : MonoBehaviour {
+
+	bool hasCollided = false;
+	string labelText = "";
+	int fontSize = 20;
+
+
+	void OnTriggerEnter(Collider c){
+		if (c.gameObject.tag == "Player") {
+			Debug.Log("Player is here");
+			hasCollided = true;
+			labelText = "Heat this up in the microwave please";
+		}
+	}
+
+	void OnGUI(){
+		GUI.skin.label.fontSize = GUI.skin.box.fontSize = GUI.skin.button.fontSize = fontSize;
+		if (hasCollided == true) {
+			GUI.Box(new Rect(140,Screen.height-50,Screen.width-300,120), labelText);
+		}
+
+		if (Input.GetKeyDown (KeyCode.E)) {
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		Debug.Log ("Player has exit the trigger");
+		hasCollided = false;
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.E)) {
+			gameObject.SetActive (false);
+		}
+	}
+}
